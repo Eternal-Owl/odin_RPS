ffa324fa9103025bbbda8e4e4d96872f1703d70f
owl <174026543+Eternal-Owl@users.noreply.github.com>
2024-10-09 10:58:51 -0500

    computer piece built

--- rps.js
+++ rps.js
@@ -1,5 +1,55 @@
-console.log("howdy");
+//scope:
+// Game of rock paper scissors in which the player is prompted to enter a value and is against a computer
+// a full game will last 5 rounds
+
+
+// Function for computer result
+let computerChoice;
+
+function getComputerChoice() {
+    computerChoice = Math.floor((Math.random() * 3)+1 );
+    console.log(computerChoice);
+    switch (computerChoice) {
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'scissors';
+            break;
+        case 3:
+            computerChoice ='paper';
+            break;
+        }
+        return computerChoice;
+    }
+getComputerChoice()
+
+//Function for Human choices
+    // Prompt for choices
+    // Capture response and store in variable
+    // then initiate logic check (next function)
 
 
 
+//Function for deciding winner
+// Need to determine who wins what 
+// Rock beats scissors
+// scissors beats paper
+// paper beats rock
+// if values === then the round is a draw
+
+
+//score keeping function
+    // define variables for:
+        // Human Score
+        // Computer Score
+        //Return Scores H - C
+
+
+// need to start next round
+
+
+
+//Debug -- Print
+console.log(computerChoice);
 
